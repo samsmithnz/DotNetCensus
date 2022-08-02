a7a8249433bd4201b4a8ff29fbe962a5321f10bc
Sam Smith <samsmithnz@gmail.com>
2022-08-02 08:51:53 -0400

    Fixed test

--- src/DotNetCensus.Tests/ConsoleAppTests.cs
+++ src/DotNetCensus.Tests/ConsoleAppTests.cs
@@ -154,7 +154,7 @@ Sample.VB6.WinApp.vbp                       \Sample.VB6.Calculator\Sample.VB6.Wi
 
             //Asset
             Assert.IsNotNull(expected);
-            Assert.AreEqual(expected.Replace(@"\",""), Environment.NewLine + sw.ToString().Replace(@"\", ""));
+            Assert.AreEqual(expected.Replace(@"\", "/"), Environment.NewLine + sw.ToString().Replace(@"\", "/"));
         }
     }
 
