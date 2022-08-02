8366a1346662ba2a1660773af81bd5004af532c8
Sam Smith <samsmithnz@gmail.com>
2022-08-01 21:33:43 -0400

    code gardening

--- src/DotNetCensus/Options.cs
+++ src/DotNetCensus/Options.cs
@@ -6,7 +6,7 @@ namespace DotNetCensus
     {
         [Option('d', "directory", Required = false, HelpText = "Root directory to search for projects")]
         public string? Directory { get; set; }
-        
+
         [Option('t', "Total", Required = false, Default = false, HelpText = "Include totals in results")]
         public bool IncludeTotals { get; set; }
         //[Option('o', "output", Required = false, HelpText = "output file to create csv file")]
