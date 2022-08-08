9129e20e1a0116c68072ad32ed036e611b31ef13
Sam Smith <samsmithnz@gmail.com>
2022-08-08 15:03:59 -0400

    CLeanup of code

--- src/DotNetCensus.Core/ProjectScanning.cs
+++ src/DotNetCensus.Core/ProjectScanning.cs
@@ -456,61 +456,6 @@ namespace DotNetCensus.Core
             }
             return language;
         }
-
-        //From: https://stackoverflow.com/questions/37294702/directoryinfo-getfiles-error-system-unathorizedaccessexception
-        public static IEnumerable<FileInfo> EnumerateFiles(string path, string? searchPattern = null)
-        {
-            if (searchPattern != null)
-            {
-                searchPattern = searchPattern ?? "*";
-            }
-            else
-            {
-                searchPattern = "*.*";
-            }
-
-            var queue = new Queue<string>();
-            queue.Enqueue(path);
-
-            do
-            {
-                path = queue.Dequeue();
-                foreach (var file in SafeEnumerateFiles(path, searchPattern))
-                {
-                    yield return new FileInfo(file);
-                }
-                foreach (var directory in SafeEnumerateDirectories(path))
-                {
-                    queue.Enqueue(directory);
-                }
-            }
-            while (queue.Any());
-        }
-
-        static IEnumerable<string> SafeEnumerateFiles(string path, string searchPattern)
-        {
-            try
-            {
-                return Directory.EnumerateFiles(path, searchPattern);
-            }
-            catch (DirectoryNotFoundException) { }
-            catch (SecurityException) { }
-            catch (UnauthorizedAccessException) { }
-
-            return Enumerable.Empty<string>();
-        }
-
-        static IEnumerable<string> SafeEnumerateDirectories(string path)
-        {
-            try
-            {
-                return Directory.EnumerateDirectories(path);
-            }
-            catch (DirectoryNotFoundException) { }
-            catch (SecurityException) { }
-            catch (UnauthorizedAccessException) { }
-
-            return Enumerable.Empty<string>();
-        }
+      
     }
 }
