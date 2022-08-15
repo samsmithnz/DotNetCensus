ec24cd3721c0adfc1565164f79954aaed1ce3c0c
Sam Smith <samsmithnz@gmail.com>
2022-08-15 14:31:23 -0400

    Updated options and veriosn

--- src/DotNetCensus/Options.cs
+++ src/DotNetCensus/Options.cs
@@ -4,14 +4,16 @@ namespace DotNetCensus
 {
     public class Options
     {
-        [Option('d', "directory", Required = false, HelpText = "Root directory to search for projects")]
+        [Option('d', "directory", Required = false, HelpText = "Root directory to search for projects. Can't be used at the same time as -r|--repo")]
         public string? Directory { get; set; }
 
         [Option('t', "total", Required = false, Default = false, HelpText = "Include totals in results")]
         public bool IncludeTotals { get; set; }
 
-        [Option('r', "raw", Required = false, Default = false, HelpText = "Show raw results, instead of an aggregate")]
-        public bool IncludeRawResults { get; set; }
+        [Option('i', "inventory", Required = false, Default = false, HelpText = "Show inventory of raw results, instead of an aggregate")]
+        public bool IncludeInventory { get; set; }
+        [Option('r', "repo", Required = false, Default = false, HelpText = "Git repo to search for projects. Can't be used at the same time as -d|--directory. Note that this was formed used by --raw - which is now achieved with -i|--inventory")]
+        public bool GitRepoUrl { get; set; }
 
         [Option('f', "file", Required = false, HelpText = "File to create CSV file")]
         public string? File { get; set; }
