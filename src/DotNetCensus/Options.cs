798515823986daa0b102a8b1735e0f67935fec21
Sam Smith <samsmithnz@gmail.com>
2022-08-15 15:45:17 -0400

    Fix to namespaces

--- src/DotNetCensus/Options.cs
+++ src/DotNetCensus/Options.cs
@@ -1,22 +1,21 @@
 ﻿using CommandLine;
 
-namespace DotNetCensus
+namespace DotNetCensus;
+
+public class Options
 {
-    public class Options
-    {
-        [Option('d', "directory", Required = false, HelpText = "Root directory to search for projects. Can't be used at the same time as -r|--repo")]
-        public string? Directory { get; set; }
+    [Option('d', "directory", Required = false, HelpText = "Root directory to search for projects. Can't be used at the same time as -r|--repo")]
+    public string? Directory { get; set; }
 
-        [Option('r', "repo", Required = false, HelpText = "Git repo to search for projects. Can't be used at the same time as -d|--directory. Note that this -r argument was formed used by --raw - which is now achieved with -i|--inventory")]
-        public string? Repo { get; set; }
+    [Option('r', "repo", Required = false, HelpText = "Git repo to search for projects. Can't be used at the same time as -d|--directory. Note that this -r argument was formed used by --raw - which is now achieved with -i|--inventory")]
+    public string? Repo { get; set; }
 
-        [Option('f', "file", Required = false, HelpText = "File to create CSV file")]
-        public string? File { get; set; }
+    [Option('f', "file", Required = false, HelpText = "File to create CSV file")]
+    public string? File { get; set; }
 
-        [Option('t', "total", Required = false, Default = false, HelpText = "Include totals in results. Ignored when used with -i|--inventory")]
-        public bool IncludeTotals { get; set; }
+    [Option('t', "total", Required = false, Default = false, HelpText = "Include totals in results. Ignored when used with -i|--inventory")]
+    public bool IncludeTotals { get; set; }
 
-        [Option('i', "inventory", Required = false, Default = false, HelpText = "Show inventory of results, instead of an aggregate")]
-        public bool IncludeInventory { get; set; }
-    }
+    [Option('i', "inventory", Required = false, Default = false, HelpText = "Show inventory of results, instead of an aggregate")]
+    public bool IncludeInventory { get; set; }
 }
--- src/DotNetCensus/Program.cs
+++ src/DotNetCensus/Program.cs
@@ -1,77 +1,62 @@
 ﻿using CommandLine;
 
-namespace DotNetCensus
+namespace DotNetCensus;
+
+public class Program
 {
-    public class Program
+    private static string? _directory;
+    private static string? _repo;
+    private static bool _includeTotals;
+    private static bool _includeInventory;
+    private static string? _file;
+
+    public static void Main(string[] args)
     {
-        private static string? _directory;
-        private static string? _repo;
-        private static bool _includeTotals;
-        private static bool _includeInventory;
-        private static string? _file;
+        //process arguments
+        ParserResult<Options>? result = Parser.Default.ParseArguments<Options>(args)
+               .WithParsed(RunOptions)
+               .WithNotParsed(HandleParseError);
 
-        public static void Main(string[] args)
+        //If there is a folder to scan, run the process against it
+        if (string.IsNullOrEmpty(_directory) == false || string.IsNullOrEmpty(_repo) == false)
         {
-            //process arguments
-            ParserResult<Options>? result = Parser.Default.ParseArguments<Options>(args)
-                   .WithParsed(RunOptions)
-                   .WithNotParsed(HandleParseError);
-
-            //If there is a folder to scan, run the process against it
-            if (string.IsNullOrEmpty(_directory) == false || string.IsNullOrEmpty(_repo) == false)
+            if (_includeInventory == true)
             {
-                if (_includeInventory == true)
-                {
-                    DataAccess.GetInventoryResults(_directory, _repo, _file);
-                }
-                else
-                {
-                    DataAccess.GetFrameworkSummary(_directory, _repo, _includeTotals, _file);
-                }
+                DataAccess.GetInventoryResults(_directory, _repo, _file);
+            }
+            else
+            {
+                DataAccess.GetFrameworkSummary(_directory, _repo, _includeTotals, _file);
             }
         }
+    }
 
-        static void RunOptions(Options opts)
+    static void RunOptions(Options opts)
+    {
+        //handle options
+        _directory = opts.Directory;
+        _repo = opts.Repo;            
+        if (_directory == null && _repo == null)
         {
-            //handle options
-            //if (string.IsNullOrEmpty(opts.Directory) == true)
-            //{
-            //    //if (string.IsNullOrEmpty(opts.RepoUrl) == false)
-            //    //{
-            //    //    _repo = opts.RepoUrl;
-            //    //}
-            //    //else
-            //    //{
-            //    _directory = Directory.GetCurrentDirectory();
-            //    //}
-            //}
-            //else
-            //{
-            //    _directory = opts.Directory;
-            //}
-            _directory = opts.Directory;
-            _repo = opts.Repo;
-            if (_directory == null && _repo == null)
-            {
-                _directory = Directory.GetCurrentDirectory();
-            }
-            _includeTotals = opts.IncludeTotals;
-            _includeInventory = opts.IncludeInventory;
-            _file = opts.File;
+            //If both directory and repo are null, use the current directory
+            _directory = Directory.GetCurrentDirectory();
         }
+        _includeTotals = opts.IncludeTotals;
+        _includeInventory = opts.IncludeInventory;
+        _file = opts.File;
+    }
 
-        static void HandleParseError(IEnumerable<Error> errs)
+    static void HandleParseError(IEnumerable<Error> errs)
+    {
+        //handle errors
+        var excList = new List<Exception>();
+        foreach (var err in errs)
         {
-            //handle errors
-            var excList = new List<Exception>();
-            foreach (var err in errs)
-            {
-                excList.Add(new ArgumentException(err.ToString()));
-            }
-            if (excList.Any())
-            {
-                throw new AggregateException(excList);
-            }
+            excList.Add(new ArgumentException(err.ToString()));
+        }
+        if (excList.Any())
+        {
+            throw new AggregateException(excList);
         }
     }
 }
