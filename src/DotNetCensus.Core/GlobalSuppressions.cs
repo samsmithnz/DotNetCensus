3137ab93cb0bd707fd8ff28d1ab6703e735ab78b
Sam Smith <samsmithnz@gmail.com>
2022-08-20 08:09:17 -0400

    fix to suppressions

--- src/DotNetCensus.Core/GlobalSuppressions.cs
+++ src/DotNetCensus.Core/GlobalSuppressions.cs
@@ -9,4 +9,5 @@ using System.Diagnostics.CodeAnalysis;
 [assembly: SuppressMessage("Style", "IDE1006:Naming Styles", Justification = "Is GitHub class", Scope = "member", Target = "~P:DotNetCensus.Core.Models.GitHub.FileResponse.path")]
 [assembly: SuppressMessage("Style", "IDE1006:Naming Styles", Justification = "Is GitHub class", Scope = "member", Target = "~P:DotNetCensus.Core.Models.GitHub.FileDetails.type")]
 [assembly: SuppressMessage("Style", "IDE1006:Naming Styles", Justification = "Is GitHub class", Scope = "member", Target = "~P:DotNetCensus.Core.Models.GitHub.FileDetails.size")]
-//[assembly: SuppressMessage("Style", "IDE0074:Use compound assignment", Justification = "Rule doesn't improve readability", Scope = "member", Target = "~M:DotNetCensus.Core.DirectoryProjectScanning.SearchProjectFile(System.IO.FileInfo,System.String,System.IO.FileInfo)~System.Collections.Generic.List{DotNetCensus.Core.Models.Project}")]
+[assembly: SuppressMessage("Style", "IDE1006:Naming Styles", Justification = "Is GitHub class", Scope = "member", Target = "~P:DotNetCensus.Core.Models.GitHub.FileDetails.name")]
+[assembly: SuppressMessage("Style", "IDE1006:Naming Styles", Justification = "Is GitHub class", Scope = "member", Target = "~P:DotNetCensus.Core.Models.GitHub.FileDetails.path")]
