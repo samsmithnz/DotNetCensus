16355d7b1905fa4768915688aecd38684924095e
Sam Smith <samsmithnz@gmail.com>
2022-08-25 16:17:46 -0400

    code gardening in test

--- src/DotNetCensus.Tests/RepoDataAccessTests.cs
+++ src/DotNetCensus.Tests/RepoDataAccessTests.cs
@@ -123,33 +123,4 @@ total frameworks                       36
         }
     }
 
-    //    [TestMethod]
-    //    public void FrameworkSummaryWithUserNameAndPasswordRepoTest()
-    //    {
-    //        //Arrange
-    //        bool includeTotals = false;
-    //        string? directory = null;
-    //        Repo? repo = new("samsmithnz", "DotNetCensus")
-    //        {
-    //            User = null,
-    //            Password = null
-    //        };
-    //        string? file = null;
-    //        if (directory != null || repo != null)
-    //        {
-    //            string expected = @"Framework      FrameworkFamily  Count  Status          
-    //-------------------------------------------------------
-    //.NET Core 3.1  .NET Core        1      EOL: 13-Dec-2022
-    //(Unknown)      (Unknown)        1      unknown         
-    //";
-
-    //            //Act
-    //            string? contents = Main.GetFrameworkSummary(directory, repo, includeTotals, file);
-
-    //            //Asset
-    //            Assert.IsNotNull(expected);
-    //            Assert.AreEqual(expected.Replace("\\", "/"), contents?.Replace("\\", "/"));
-    //        }
-    //    }
-
 }
