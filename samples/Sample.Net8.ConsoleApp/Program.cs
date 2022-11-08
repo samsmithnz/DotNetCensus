0534e49da9eae2edc03ac2f6b9e581233a5b6e98
Sam Smith <samsmithnz@gmail.com>
2022-11-08 13:02:06 -0500

    Added .NET 8 sample

---
+++ samples/Sample.Net8.ConsoleApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Sample.Net5.ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
