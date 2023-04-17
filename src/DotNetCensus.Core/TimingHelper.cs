6250e9213c7f614d75e7f1ace5faa3df3c3075ed
Sam Smith <samsmithnz@gmail.com>
2023-04-17 15:41:11 -0400

    Added ms to timing

--- src/DotNetCensus.Core/TimingHelper.cs
+++ src/DotNetCensus.Core/TimingHelper.cs
@@ -6,11 +6,15 @@
         {
             if (timespan.TotalMinutes > 1)
             {
-                return $"{timespan.TotalMinutes.ToString()}:{timespan.TotalSeconds.ToString("00")} mins";
+                return $"{timespan.TotalMinutes.ToString()}:{timespan.TotalSeconds.ToString("00")}.{timespan.TotalMilliseconds.ToString("0")} mins";
+            }
+            else if (timespan.TotalSeconds > 1) 
+            {
+                return $"{timespan.TotalSeconds.ToString("0")}.{timespan.TotalMilliseconds.ToString("0")} seconds";
             }
             else
             {
-                return $"{timespan.TotalSeconds.ToString("0")} seconds";
+                return $"{timespan.TotalMilliseconds.ToString("0")} ms";
             }
         }
     }
