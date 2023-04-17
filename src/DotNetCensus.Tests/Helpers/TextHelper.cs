5171f70b9ddb3b3120c7bcb91f278e33d0f806e4
Sam Smith <samsmithnz@gmail.com>
2023-04-17 15:50:37 -0400

    Fixed tests after adding timing

---
+++ src/DotNetCensus.Tests/Helpers/TextHelper.cs
@@ -0,0 +1,16 @@
+﻿namespace DotNetCensus.Tests.Helpers
+{
+    public static class TextHelper
+    {
+        public static string CleanTimingFromResult(string str)
+        {
+            //All results have a timing value at the end of the line. 
+            //Remove this, for example: "Time to process: 23 ms"
+            //Also correctly processes the end of line characters
+            str = str.Remove(str.LastIndexOf(Environment.NewLine));
+            str = str.Remove(str.LastIndexOf(Environment.NewLine));
+            str += Environment.NewLine;
+            return str;
+        }
+    }
+}
