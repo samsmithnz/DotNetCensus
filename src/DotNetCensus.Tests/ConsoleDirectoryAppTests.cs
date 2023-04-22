1b1fca5615048a5960e52da424d6192d3519045e
Sam Smith <samsmithnz@gmail.com>
2023-04-22 09:01:35 -0400

    updated test

--- src/DotNetCensus.Tests/ConsoleDirectoryAppTests.cs
+++ src/DotNetCensus.Tests/ConsoleDirectoryAppTests.cs
@@ -91,35 +91,35 @@ public class ConsoleDirectoryAppTests : DirectoryBasedTests
             string[] parameters = new string[] { "-d", SamplesPath, "-t", "-f", file };
             StringWriter sw = new();
             string expected = @"Framework,FrameworkFamily,Count,Status
-    .NET 5.0,.NET,2,deprecated
-    .NET 6.0,.NET,4,supported
-    .NET 6.0-android,.NET,1,supported
-    .NET 6.0-ios,.NET,1,supported
-    .NET 6.0-maccatalyst,.NET,1,supported
-    .NET 7.0,.NET,2,supported
-    .NET 8.0,.NET,5,in preview
-    .NET Core 1.0,.NET Core,1,deprecated
-    .NET Core 1.1,.NET Core,1,deprecated
-    .NET Core 2.0,.NET Core,1,deprecated
-    .NET Core 2.1,.NET Core,1,deprecated
-    .NET Core 2.2,.NET Core,1,deprecated
-    .NET Core 3.0,.NET Core,2,deprecated
-    .NET Core 3.1,.NET Core,3,deprecated
-    .NET Framework 1.0,.NET Framework,1,deprecated
-    .NET Framework 1.1,.NET Framework,1,deprecated
-    .NET Framework 2.0,.NET Framework,1,deprecated
-    .NET Framework 3.5,.NET Framework,2,EOL: 9-Jan-2029
-    .NET Framework 4.0,.NET Framework,1,deprecated
-    .NET Framework 4.5,.NET Framework,1,deprecated
-    .NET Framework 4.6.1,.NET Framework,1,deprecated
-    .NET Framework 4.6.2,.NET Framework,1,supported
-    .NET Framework 4.7.1,.NET Framework,1,supported
-    .NET Framework 4.7.2,.NET Framework,3,supported
-    .NET Standard 2.0,.NET Standard,3,supported
-    (Unknown),(Unknown),2,unknown
-    Visual Basic 6,Visual Basic 6,1,deprecated
-    total frameworks,,45,
-    ";
+.NET 5.0,.NET,2,deprecated
+.NET 6.0,.NET,4,supported
+.NET 6.0-android,.NET,1,supported
+.NET 6.0-ios,.NET,1,supported
+.NET 6.0-maccatalyst,.NET,1,supported
+.NET 7.0,.NET,2,supported
+.NET 8.0,.NET,5,in preview
+.NET Core 1.0,.NET Core,1,deprecated
+.NET Core 1.1,.NET Core,1,deprecated
+.NET Core 2.0,.NET Core,1,deprecated
+.NET Core 2.1,.NET Core,1,deprecated
+.NET Core 2.2,.NET Core,1,deprecated
+.NET Core 3.0,.NET Core,2,deprecated
+.NET Core 3.1,.NET Core,3,deprecated
+.NET Framework 1.0,.NET Framework,1,deprecated
+.NET Framework 1.1,.NET Framework,1,deprecated
+.NET Framework 2.0,.NET Framework,1,deprecated
+.NET Framework 3.5,.NET Framework,2,EOL: 9-Jan-2029
+.NET Framework 4.0,.NET Framework,1,deprecated
+.NET Framework 4.5,.NET Framework,1,deprecated
+.NET Framework 4.6.1,.NET Framework,1,deprecated
+.NET Framework 4.6.2,.NET Framework,1,supported
+.NET Framework 4.7.1,.NET Framework,1,supported
+.NET Framework 4.7.2,.NET Framework,3,supported
+.NET Standard 2.0,.NET Standard,3,supported
+(Unknown),(Unknown),2,unknown
+Visual Basic 6,Visual Basic 6,1,deprecated
+total frameworks,,45,
+";
 
             //Act
             Console.SetOut(sw);
