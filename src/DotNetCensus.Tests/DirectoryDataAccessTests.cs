d1c424040b75c44f8ac2401ca0b4657d3be3fe8b
Sam Smith <samsmithnz@gmail.com>
2023-04-22 20:01:50 -0400

    Added new test

--- src/DotNetCensus.Tests/DirectoryDataAccessTests.cs
+++ src/DotNetCensus.Tests/DirectoryDataAccessTests.cs
@@ -374,10 +374,39 @@ total frameworks,,33,
 
 
     [TestMethod]
-    public void FrameworkSummaryMultipleDirectoryMAUICalculatorResultsTest()
+    public void FrameworkSummaryMAUICalculatorResultsTest()
     {
         //Arrange
         string? directory = SamplesPath + @"/Sample.Net6.MAUI.Calculator";
+        Repo? repo = null;
+        bool includeTotals = true;
+        string? file = null;
+        if (directory != null || repo != null)
+        {
+            string expected = @"Framework             FrameworkFamily  Count  Status   
+-------------------------------------------------------
+.NET 6.0-android      .NET             1      supported
+.NET 6.0-ios          .NET             1      supported
+.NET 6.0-maccatalyst  .NET             1      supported
+total frameworks                       3               
+";
+
+            //Act
+            string? contents = Main.GetFrameworkSummaryAsString(directory, repo, includeTotals, file);
+
+            //Asset
+            Assert.IsNotNull(expected);
+            Assert.AreEqual(expected.Replace("\\", "/"), contents?.Replace("\\", "/"));
+        }
+    }
+
+
+
+    [TestMethod]
+    public void FrameworkSummaryXamarinResultsTest()
+    {
+        //Arrange
+        string? directory = SamplesPath + @"/Sample.Xamarin";
         Repo? repo = null;
         bool includeTotals = true;
         string? file = null;
