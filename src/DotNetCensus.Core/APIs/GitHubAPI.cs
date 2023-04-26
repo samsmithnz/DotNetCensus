23ac8bcf0e724095eab22c6ba6966a8c18ebc3d2
Sam Smith <samsmithnz@gmail.com>
2023-04-26 15:52:55 -0400

    Updated to generic files

--- src/DotNetCensus.Core/APIs/GitHubAPI.cs
+++ src/DotNetCensus.Core/APIs/GitHubAPI.cs
@@ -39,7 +39,7 @@ namespace DotNetCensus.Core.APIs
                         }
                         if (ProjectClassification.IsProjectFile(fileInfo.Name) ||
                             ProjectClassification.IsProjectFile(fileInfo.Name, false) ||
-                            fileInfo.Name.ToLower() == "directory.build.props")
+                            fileInfo.Extension.ToLower() == "directory.build.props")
                         {
                             results.Add(new Project()
                             {
