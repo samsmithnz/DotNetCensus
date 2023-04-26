52236a17b9afd05ea82fa98cae3caca9d0ee40b0
Sam Smith <samsmithnz@gmail.com>
2023-04-26 16:24:43 -0400

    updated test

--- src/DotNetCensus.Tests/SampleDataUnitTests.cs
+++ src/DotNetCensus.Tests/SampleDataUnitTests.cs
@@ -114,7 +114,7 @@ public class SampleDataUnitTests : DirectoryBasedTests
         //Asset
         Assert.IsNotNull(results);
         Assert.AreEqual(4, results.Count);
-        Assert.AreEqual(38, results[0].Count);
+        Assert.AreEqual(39, results[0].Count);
         Assert.AreEqual("csharp", results[0].Language);
         Assert.AreEqual(2, results[1].Count);
         Assert.AreEqual("fsharp", results[1].Language);
