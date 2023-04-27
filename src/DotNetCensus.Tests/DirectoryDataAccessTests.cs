4c102ed17ddae7e6aaaec021e6635de88cfc84ac
Sam Smith <samsmithnz@gmail.com>
2023-04-27 06:05:52 -0400

    udpated tests

--- src/DotNetCensus.Tests/DirectoryDataAccessTests.cs
+++ src/DotNetCensus.Tests/DirectoryDataAccessTests.cs
@@ -172,7 +172,7 @@ total frameworks                       48
 .NET 6.0-ios,.NET,1,supported
 .NET 6.0-maccatalyst,.NET,1,supported
 .NET 7.0,.NET,2,supported
-.NET 8.0,.NET,6,in preview
+.NET 8.0,.NET,7,in preview
 .NET Core 1.0,.NET Core,1,deprecated
 .NET Core 1.1,.NET Core,1,deprecated
 .NET Core 2.0,.NET Core,1,deprecated
@@ -194,7 +194,7 @@ total frameworks                       48
 .NET Standard 2.0,.NET Standard,3,supported
 (Unknown),(Unknown),2,unknown
 Visual Basic 6,Visual Basic 6,1,deprecated
-total frameworks,,47,
+total frameworks,,48,
 ";
 
             //Act
