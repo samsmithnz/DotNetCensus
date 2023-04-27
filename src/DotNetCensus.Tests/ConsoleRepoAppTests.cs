caeec7c0d07487c61bb7506a77c4669c01540855
Sam Smith <samsmithnz@gmail.com>
2023-04-26 21:03:25 -0400

    updated tests

--- src/DotNetCensus.Tests/ConsoleRepoAppTests.cs
+++ src/DotNetCensus.Tests/ConsoleRepoAppTests.cs
@@ -27,7 +27,7 @@ public class ConsoleRepoAppTests : RepoBasedTests
 .NET 6.0-ios          .NET             1      supported      
 .NET 6.0-maccatalyst  .NET             1      supported      
 .NET 7.0              .NET             5      supported      
-.NET 8.0              .NET             5      in preview     
+.NET 8.0              .NET             6      in preview     
 .NET Core 1.0         .NET Core        1      deprecated     
 .NET Core 1.1         .NET Core        1      deprecated     
 .NET Core 2.0         .NET Core        1      deprecated     
@@ -49,7 +49,7 @@ public class ConsoleRepoAppTests : RepoBasedTests
 .NET Standard 2.0     .NET Standard    3      supported      
 (Unknown)             (Unknown)        2      unknown        
 Visual Basic 6        Visual Basic 6   1      deprecated     
-total frameworks                       51                    
+total frameworks                       52                    
 
 ";
 
