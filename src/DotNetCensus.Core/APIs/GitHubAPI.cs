3e94642b9166bc2f11d822f64c40a33b7d41c10a
Sam Smith <samsmithnz@gmail.com>
2023-04-28 05:57:29 -0400

    code gardening

--- src/DotNetCensus.Core/APIs/GitHubAPI.cs
+++ src/DotNetCensus.Core/APIs/GitHubAPI.cs
@@ -103,7 +103,7 @@ namespace DotNetCensus.Core.APIs
 
         private static bool IsBase64String(string base64)
         {
-            Span<byte> buffer = new Span<byte>(new byte[base64.Length]);
+            Span<byte> buffer = new(new byte[base64.Length]);
             return Convert.TryFromBase64String(base64, buffer, out int bytesParsed);
         }
 
