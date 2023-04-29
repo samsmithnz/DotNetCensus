c31db246d537524d6b3d055af18b7f649e137d2f
Sam Smith <samsmithnz@gmail.com>
2023-04-28 20:46:43 -0400

    updated test

--- src/DotNetCensus.Tests/DirectoryDataAccessTests.cs
+++ src/DotNetCensus.Tests/DirectoryDataAccessTests.cs
@@ -185,18 +185,19 @@ total frameworks                       51
 .NET Framework 1.0,.NET Framework,1,deprecated
 .NET Framework 1.1,.NET Framework,1,deprecated
 .NET Framework 2.0,.NET Framework,1,deprecated
-.NET Framework 3.5,.NET Framework,2,EOL: 9-Jan-2029
-.NET Framework 4.0,.NET Framework,1,deprecated
+.NET Framework 3.5,.NET Framework,3,EOL: 9-Jan-2029
+.NET Framework 4.0,.NET Framework,2,deprecated
 .NET Framework 4.5,.NET Framework,1,deprecated
 .NET Framework 4.5.1,.NET Framework,1,deprecated
+.NET Framework 4.5.2,.NET Framework,1,deprecated
 .NET Framework 4.6.1,.NET Framework,1,deprecated
-.NET Framework 4.6.2,.NET Framework,1,supported
+.NET Framework 4.6.2,.NET Framework,1,EOL: 12-Jan-2027
 .NET Framework 4.7.1,.NET Framework,1,supported
 .NET Framework 4.7.2,.NET Framework,3,supported
 .NET Standard 2.0,.NET Standard,3,supported
 (Unknown),(Unknown),2,unknown
 Visual Basic 6,Visual Basic 6,1,deprecated
-total frameworks,,48,
+total frameworks,,51,
 ";
 
             //Act
