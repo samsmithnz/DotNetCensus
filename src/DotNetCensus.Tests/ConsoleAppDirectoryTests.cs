c64e0debfd84d8dbbebf2d0418f8c37fc04035d2
Sam Smith <samsmithnz@gmail.com>
2023-06-19 06:52:17 -0400

    experimenting with tests

--- src/DotNetCensus.Tests/ConsoleAppDirectoryTests.cs
+++ src/DotNetCensus.Tests/ConsoleAppDirectoryTests.cs
@@ -1,4 +1,5 @@
 using DotNetCensus.Tests.Helpers;
+using System.Diagnostics;
 
 namespace DotNetCensus.Tests;
 
@@ -109,7 +110,7 @@ public class ConsoleAppDirectoryTests : DirectoryBasedTests
             string file = "test.txt";
             string[] parameters = new string[] { "-d", SamplesPath, "-t", "-f", file };
             StringWriter sw = new();
-        string expected = @"Framework,FrameworkFamily,Count,Status
+            string expected = @"Framework,FrameworkFamily,Count,Status
 .NET 5.0,.NET,2,deprecated
 .NET 6.0,.NET,4,supported
 .NET 6.0-android,.NET,1,supported
@@ -168,5 +169,75 @@ total frameworks,,64,
             Assert.AreEqual(expected.Replace("\\", "/"), contents?.Replace("\\", "/"));
         }
     }
+
+    //    [TestMethod]
+    //    public void RunConsoleAppWithTotalsNewTest()
+    //    {
+    //        //Arrange
+    //        if (SamplesPath != null && ConsoleExePath != null)
+    //        {
+    //            string arguments = $"-d {SamplesPath} -t";
+    //            Process? process = StartApplication(arguments);
+    //            string? actual = null;
+    //            string expected = @"Framework,FrameworkFamily,Count,Status
+    //.NET 5.0,.NET,2,deprecated
+    //.NET 6.0,.NET,4,supported
+    //.NET 6.0-android,.NET,1,supported
+    //.NET 6.0-ios,.NET,1,supported
+    //.NET 6.0-maccatalyst,.NET,1,supported
+    //.NET 7.0,.NET,2,supported
+    //.NET 8.0,.NET,7,in preview
+    //.NET Core 1.0,.NET Core,1,deprecated
+    //.NET Core 1.1,.NET Core,1,deprecated
+    //.NET Core 2.0,.NET Core,1,deprecated
+    //.NET Core 2.1,.NET Core,1,deprecated
+    //.NET Core 2.2,.NET Core,1,deprecated
+    //.NET Core 3.0,.NET Core,2,deprecated
+    //.NET Core 3.1,.NET Core,3,deprecated
+    //.NET Framework 1.0,.NET Framework,1,deprecated
+    //.NET Framework 1.1,.NET Framework,1,deprecated
+    //.NET Framework 2.0,.NET Framework,1,deprecated
+    //.NET Framework 3.0,.NET Framework,1,deprecated
+    //.NET Framework 3.5,.NET Framework,3,EOL: 9-Jan-2029
+    //.NET Framework 4.0,.NET Framework,2,deprecated
+    //.NET Framework 4.5,.NET Framework,1,deprecated
+    //.NET Framework 4.5.1,.NET Framework,1,deprecated
+    //.NET Framework 4.5.2,.NET Framework,1,deprecated
+    //.NET Framework 4.6,.NET Framework,1,deprecated
+    //.NET Framework 4.6.1,.NET Framework,1,deprecated
+    //.NET Framework 4.6.2,.NET Framework,1,EOL: 12-Jan-2027
+    //.NET Framework 4.7,.NET Framework,1,supported
+    //.NET Framework 4.7.1,.NET Framework,1,supported
+    //.NET Framework 4.7.2,.NET Framework,3,supported
+    //.NET Framework 4.8,.NET Framework,1,supported
+    //.NET Framework 4.8.1,.NET Framework,1,supported
+    //.NET Standard 1.0,.NET Standard,1,supported
+    //.NET Standard 1.1,.NET Standard,1,supported
+    //.NET Standard 1.2,.NET Standard,1,supported
+    //.NET Standard 1.3,.NET Standard,1,supported
+    //.NET Standard 1.4,.NET Standard,1,supported
+    //.NET Standard 1.5,.NET Standard,1,supported
+    //.NET Standard 1.6,.NET Standard,1,supported
+    //.NET Standard 2.0,.NET Standard,3,supported
+    //.NET Standard 2.1,.NET Standard,1,supported
+    //(Unknown),(Unknown),2,unknown
+    //Visual Basic 6,Visual Basic 6,1,deprecated
+    //total frameworks,,64,
+    //";
+
+    //            //Act
+    //            if (process != null)
+    //            {
+    //                Task<string?> outputTask = WaitForResponse(process);
+    //                outputTask.Wait();
+    //                actual = outputTask.Result;
+    //            }
+
+    //            //Asset
+    //            //actual = TextHelper.CleanTimingFromResult(actual);
+    //            Assert.IsNotNull(actual);
+    //            Assert.AreEqual(expected.Replace("\\", "/"), actual?.Replace("\\", "/"));
+    //        }
+    //    }
 
 }
