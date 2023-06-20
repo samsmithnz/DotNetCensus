c72880db1d2d165cb4fdde1500f4790f525cc490
Sam Smith <samsmithnz@gmail.com>
2023-06-20 15:49:14 -0400

    Refactored the main function to resolve functional tests stepping on each other.

---
+++ src/DotNetCensus/CommandLineParser.cs
@@ -0,0 +1,60 @@
+﻿using CommandLine;
+using DotNetCensus.Core.Models;
+
+namespace DotNetCensus
+{
+    public class CommandLineParser
+    {
+        public string? Directory { get; set; }
+        public Repo? Repo { get; set; }
+        public bool IncludeTotals { get; set; }
+        public bool IncludeInventory { get; set; }
+        public string? OutputFile { get; set; }
+
+        public CommandLineParser(string[] args)
+        {
+            //process arguments
+            Parser.Default.ParseArguments<Options>(args)
+                .WithParsed(RunOptions)
+                .WithNotParsed(HandleParseError);
+        }
+
+        internal void RunOptions(Options opts)
+        {
+            //setup the GitHub repo details
+            if (opts.Owner != null)
+            {
+                Repo = new Repo(opts.Owner, opts.Repo)
+                {
+                    User = opts.User,
+                    Password = opts.Password
+                };
+            }
+            //Setup working directory details
+            Directory = opts.Directory;
+            if (Directory == null && Repo == null)
+            {
+                //If both directory and repo are null, use the current directory
+                Directory = System.IO.Directory.GetCurrentDirectory();
+            }
+            //misc options
+            IncludeTotals = opts.IncludeTotals;
+            IncludeInventory = opts.IncludeInventory;
+            OutputFile = opts.File;
+        }
+
+        internal void HandleParseError(IEnumerable<Error> errs)
+        {
+            //handle errors
+            List<Exception> excList = new();
+            foreach (var err in errs)
+            {
+                excList.Add(new ArgumentException(err.ToString()));
+            }
+            if (excList.Any())
+            {
+                throw new AggregateException(excList);
+            }
+        }
+    }
+}
--- src/DotNetCensus/Program.cs
+++ src/DotNetCensus/Program.cs
@@ -1,72 +1,22 @@
-﻿using CommandLine;
-using DotNetCensus.Core.Models;
-
-namespace DotNetCensus;
+﻿namespace DotNetCensus;
 
 public class Program
 {
-    private static string? _directory;
-    private static Repo? _repo;
-    private static bool _includeTotals;
-    private static bool _includeInventory;
-    private static string? _file;
-
     public static void Main(string[] args)
     {
-        //process arguments
-        Parser.Default.ParseArguments<Options>(args)
-            .WithParsed(RunOptions)
-            .WithNotParsed(HandleParseError);
+        CommandLineParser newClass = new(args);
 
         //If there is a folder to scan, run the process against it
-        if (!string.IsNullOrEmpty(_directory) || _repo != null)
+        if (!string.IsNullOrEmpty(newClass.Directory) || newClass.Repo != null)
         {
-            if (_includeInventory)
+            if (newClass.IncludeInventory)
             {
-                Core.Main.GetInventoryResultsAsString(_directory, _repo, _file);
+                Core.Main.GetInventoryResultsAsString(newClass.Directory, newClass.Repo, newClass.OutputFile);
             }
             else
             {
-                Core.Main.GetFrameworkSummaryAsString(_directory, _repo, _includeTotals, _file);
+                Core.Main.GetFrameworkSummaryAsString(newClass.Directory, newClass.Repo, newClass.IncludeTotals, newClass.OutputFile);
             }
-        }
-    }
-
-    static void RunOptions(Options opts)
-    {
-        //setup the GitHub repo details
-        if (opts.Owner != null)
-        {
-            _repo = new Repo(opts.Owner, opts.Repo)
-            {
-                User = opts.User,
-                Password = opts.Password
-            };
-        }
-        //Setup working directory details
-        _directory = opts.Directory;
-        if (_directory == null && _repo == null)
-        {
-            //If both directory and repo are null, use the current directory
-            _directory = Directory.GetCurrentDirectory();
-        }
-        //misc options
-        _includeTotals = opts.IncludeTotals;
-        _includeInventory = opts.IncludeInventory;
-        _file = opts.File;
-    }
-
-    static void HandleParseError(IEnumerable<Error> errs)
-    {
-        //handle errors
-        List<Exception> excList = new();
-        foreach (var err in errs)
-        {
-            excList.Add(new ArgumentException(err.ToString()));
-        }
-        if (excList.Any())
-        {
-            throw new AggregateException(excList);
         }
     }
 }
