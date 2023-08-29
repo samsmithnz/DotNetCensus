bdcdc3368a952876e3302673cb31f0c2db4c3a82
Sam Smith <samsmithnz@gmail.com>
2023-08-29 19:50:40 -0400

    updated test

--- src/DotNetCensus.Tests/DirectoryDataAccessTests.cs
+++ src/DotNetCensus.Tests/DirectoryDataAccessTests.cs
@@ -37,50 +37,50 @@ public class DirectoryDataAccessTests : DirectoryBasedTests
         string? file = null;
         if (directory != null || repo != null)
         {
-            string expected = @"Framework             FrameworkFamily  Count  Status          
---------------------------------------------------------------
-.NET 5.0              .NET             2      deprecated      
-.NET 6.0              .NET             4      supported       
-.NET 6.0-android      .NET             1      supported       
-.NET 6.0-ios          .NET             1      supported       
-.NET 6.0-maccatalyst  .NET             1      supported       
-.NET 7.0              .NET             2      supported       
-.NET 8.0              .NET             7      in preview      
-.NET Core 1.0         .NET Core        1      deprecated      
-.NET Core 1.1         .NET Core        1      deprecated      
-.NET Core 2.0         .NET Core        1      deprecated      
-.NET Core 2.1         .NET Core        1      deprecated      
-.NET Core 2.2         .NET Core        1      deprecated      
-.NET Core 3.0         .NET Core        2      deprecated      
-.NET Core 3.1         .NET Core        3      deprecated      
-.NET Framework 1.0    .NET Framework   1      deprecated      
-.NET Framework 1.1    .NET Framework   1      deprecated      
-.NET Framework 2.0    .NET Framework   1      deprecated      
-.NET Framework 3.0    .NET Framework   1      deprecated      
-.NET Framework 3.5    .NET Framework   3      EOL: 9-Jan-2029 
-.NET Framework 4.0    .NET Framework   2      deprecated      
-.NET Framework 4.5    .NET Framework   1      deprecated      
-.NET Framework 4.5.1  .NET Framework   1      deprecated      
-.NET Framework 4.5.2  .NET Framework   1      deprecated      
-.NET Framework 4.6    .NET Framework   1      deprecated      
-.NET Framework 4.6.1  .NET Framework   1      deprecated      
-.NET Framework 4.6.2  .NET Framework   1      EOL: 12-Jan-2027
-.NET Framework 4.7    .NET Framework   1      supported       
-.NET Framework 4.7.1  .NET Framework   1      supported       
-.NET Framework 4.7.2  .NET Framework   3      supported       
-.NET Framework 4.8    .NET Framework   1      supported       
-.NET Framework 4.8.1  .NET Framework   1      supported       
-.NET Standard 1.0     .NET Standard    1      supported       
-.NET Standard 1.1     .NET Standard    1      supported       
-.NET Standard 1.2     .NET Standard    1      supported       
-.NET Standard 1.3     .NET Standard    1      supported       
-.NET Standard 1.4     .NET Standard    1      supported       
-.NET Standard 1.5     .NET Standard    1      supported       
-.NET Standard 1.6     .NET Standard    1      supported       
-.NET Standard 2.0     .NET Standard    3      supported       
-.NET Standard 2.1     .NET Standard    1      supported       
-(Unknown)             (Unknown)        2      unknown         
-Visual Basic 6        Visual Basic 6   1      deprecated      
+            string expected = @"| Framework            | FrameworkFamily | Count | Status           |
+|----------------------|-----------------|-------|------------------|
+| .NET 5.0             | .NET            | 2     | deprecated       |
+| .NET 6.0             | .NET            | 4     | supported        |
+| .NET 6.0-android     | .NET            | 1     | supported        |
+| .NET 6.0-ios         | .NET            | 1     | supported        |
+| .NET 6.0-maccatalyst | .NET            | 1     | supported        |
+| .NET 7.0             | .NET            | 2     | supported        |
+| .NET 8.0             | .NET            | 7     | in preview       |
+| .NET Core 1.0        | .NET Core       | 1     | deprecated       |
+| .NET Core 1.1        | .NET Core       | 1     | deprecated       |
+| .NET Core 2.0        | .NET Core       | 1     | deprecated       |
+| .NET Core 2.1        | .NET Core       | 1     | deprecated       |
+| .NET Core 2.2        | .NET Core       | 1     | deprecated       |
+| .NET Core 3.0        | .NET Core       | 2     | deprecated       |
+| .NET Core 3.1        | .NET Core       | 3     | deprecated       |
+| .NET Framework 1.0   | .NET Framework  | 1     | deprecated       |
+| .NET Framework 1.1   | .NET Framework  | 1     | deprecated       |
+| .NET Framework 2.0   | .NET Framework  | 1     | deprecated       |
+| .NET Framework 3.0   | .NET Framework  | 1     | deprecated       |
+| .NET Framework 3.5   | .NET Framework  | 3     | EOL: 9-Jan-2029  |
+| .NET Framework 4.0   | .NET Framework  | 2     | deprecated       |
+| .NET Framework 4.5   | .NET Framework  | 1     | deprecated       |
+| .NET Framework 4.5.1 | .NET Framework  | 1     | deprecated       |
+| .NET Framework 4.5.2 | .NET Framework  | 1     | deprecated       |
+| .NET Framework 4.6   | .NET Framework  | 1     | deprecated       |
+| .NET Framework 4.6.1 | .NET Framework  | 1     | deprecated       |
+| .NET Framework 4.6.2 | .NET Framework  | 1     | EOL: 12-Jan-2027 |
+| .NET Framework 4.7   | .NET Framework  | 1     | supported        |
+| .NET Framework 4.7.1 | .NET Framework  | 1     | supported        |
+| .NET Framework 4.7.2 | .NET Framework  | 3     | supported        |
+| .NET Framework 4.8   | .NET Framework  | 1     | supported        |
+| .NET Framework 4.8.1 | .NET Framework  | 1     | supported        |
+| .NET Standard 1.0    | .NET Standard   | 1     | supported        |
+| .NET Standard 1.1    | .NET Standard   | 1     | supported        |
+| .NET Standard 1.2    | .NET Standard   | 1     | supported        |
+| .NET Standard 1.3    | .NET Standard   | 1     | supported        |
+| .NET Standard 1.4    | .NET Standard   | 1     | supported        |
+| .NET Standard 1.5    | .NET Standard   | 1     | supported        |
+| .NET Standard 1.6    | .NET Standard   | 1     | supported        |
+| .NET Standard 2.0    | .NET Standard   | 3     | supported        |
+| .NET Standard 2.1    | .NET Standard   | 1     | supported        |
+| (Unknown)            | (Unknown)       | 2     | unknown          |
+| Visual Basic 6       | Visual Basic 6  | 1     | deprecated       |
 ";
 
             //Act
