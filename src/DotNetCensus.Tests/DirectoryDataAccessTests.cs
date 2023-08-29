6cd5bd66c936e66db120ded6d62f084ae5feccb8
Sam Smith <samsmithnz@gmail.com>
2023-08-29 19:49:31 -0400

    updated test

--- src/DotNetCensus.Tests/DirectoryDataAccessTests.cs
+++ src/DotNetCensus.Tests/DirectoryDataAccessTests.cs
@@ -519,10 +519,10 @@ total frameworks                       3
         string? file = null;
         if (directory != null || repo != null)
         {
-            string expected = @"Framework         FrameworkFamily  Count  Status    
-----------------------------------------------------
-.NET 8.0          .NET             1      in preview
-total frameworks                   1                
+            string expected = @"| Framework        | FrameworkFamily | Count | Status     |
+|------------------|-----------------|-------|------------|
+| .NET 8.0         | .NET            | 1     | in preview |
+| total frameworks |                 | 1     |            |
 ";
 
             //Act
