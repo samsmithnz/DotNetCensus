c79f68a08176df84a4be01c056cb3a663894f3cc
Sam Smith <samsmithnz@gmail.com>
2023-11-14 17:13:54 -0500

    Fixed test

--- src/DotNetCensus.Tests/DirectoryDataAccessTests.cs
+++ src/DotNetCensus.Tests/DirectoryDataAccessTests.cs
@@ -44,8 +44,9 @@ public class DirectoryDataAccessTests : DirectoryBasedTests
 | .NET 6.0-android     | .NET            | 1     | supported        |
 | .NET 6.0-ios         | .NET            | 1     | supported        |
 | .NET 6.0-maccatalyst | .NET            | 1     | supported        |
-| .NET 7.0             | .NET            | 2     | supported        |
-| .NET 8.0             | .NET            | 7     | in preview       |
+| .NET 7.0             | .NET            | 2     | EOL: 14-May-2024 |
+| .NET 8.0             | .NET            | 7     | supported        |
+| .NET 9.0             | .NET            | 1     | in preview       |
 | .NET Core 1.0        | .NET Core       | 1     | deprecated       |
 | .NET Core 1.1        | .NET Core       | 1     | deprecated       |
 | .NET Core 2.0        | .NET Core       | 1     | deprecated       |
