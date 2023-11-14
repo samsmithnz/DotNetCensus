8892b3e4a99b1dcfa08cd25c378517e72f274b7b
Sam Smith <samsmithnz@gmail.com>
2023-11-14 17:35:01 -0500

    Fixed test

--- src/DotNetCensus.Tests/ConsoleAppOrganizationTests.cs
+++ src/DotNetCensus.Tests/ConsoleAppOrganizationTests.cs
@@ -20,13 +20,13 @@ public class ConsoleAppOrganizationTests : RepoBasedTests
                 "-d", Path.GetTempPath(),
                 "-t" };
             StringWriter sw = new();
-            string expected = @"| Framework            | FrameworkFamily | Count | Status    |
-|----------------------|-----------------|-------|-----------|
-| .NET 7.0             | .NET            | 8     | supported |
-| .NET 7.0-windows     | .NET            | 2     | supported |
-| .NET Framework 4.7.2 | .NET Framework  | 1     | supported |
-| total frameworks     |                 | 11    |           |
-
+            string expected = @"| Framework            | FrameworkFamily | Count | Status           |
+|----------------------|-----------------|-------|------------------|
+| .NET 7.0             | .NET            | 8     | EOL: 14-May-2024 |
+| .NET 7.0-windows     | .NET            | 2     | EOL: 14-May-2024 |
+| .NET Framework 4.7.2 | .NET Framework  | 1     | supported        |
+| total frameworks     |                 | 11    |                  |
+    
 ";
 
             //Act
