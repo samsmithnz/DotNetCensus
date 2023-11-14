7a8287fe2d97918c6587e8ed1d90a9e0a66cd956
Sam Smith <samsmithnz@gmail.com>
2023-11-14 17:18:29 -0500

    fixed test

--- src/DotNetCensus.Tests/SampleDataUnitTests.cs
+++ src/DotNetCensus.Tests/SampleDataUnitTests.cs
@@ -149,7 +149,7 @@ public class SampleDataUnitTests : DirectoryBasedTests
         Assert.AreEqual("vb.net", results[2].Language);
         Assert.AreEqual(1, results[3].Count);
         Assert.AreEqual("vb6", results[3].Language);
-        Assert.AreEqual(64, results[4].Count);
+        Assert.AreEqual(65, results[4].Count);
         Assert.AreEqual("total languages", results[4].Language);
     }
 
