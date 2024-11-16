b37ef87fb952b05b29e52a11437abefb887eba7c
Sam Smith <samsmithnz@gmail.com>
2024-11-16 17:54:39 -0500

    Update framework statuses in ConsoleAppRepoTests.cs
    Updated statuses in expected output string:
    - Changed .NET 6.0, .NET 6.0-android, .NET 6.0-ios, and .NET 6.0-maccatalyst from "supported" to "deprecated".
    - Changed .NET 9.0 from "in preview" to "supported".
    - Added a semicolon at the end of the expected output string.

--- src/DotNetCensus.Tests/DirectoryDataAccessTests.cs
+++ src/DotNetCensus.Tests/DirectoryDataAccessTests.cs
@@ -497,12 +497,12 @@ total frameworks,,65,
         string? file = null;
         if (directory != null || repo != null)
         {
-            string expected = @"| Framework            | FrameworkFamily | Count | Status    |
-|----------------------|-----------------|-------|-----------|
-| .NET 6.0-android     | .NET            | 1     | supported |
-| .NET 6.0-ios         | .NET            | 1     | supported |
-| .NET 6.0-maccatalyst | .NET            | 1     | supported |
-| total frameworks     |                 | 3     |           |
+            string expected = @"| Framework            | FrameworkFamily | Count | Status     |
+|----------------------|-----------------|-------|------------|
+| .NET 6.0-android     | .NET            | 1     | deprecated |
+| .NET 6.0-ios         | .NET            | 1     | deprecated |
+| .NET 6.0-maccatalyst | .NET            | 1     | deprecated |
+| total frameworks     |                 | 3     |            |
 ";
 
             //Act
